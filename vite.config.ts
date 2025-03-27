edd3ee8fb0d0dae874d37397c0a8338c5a9473a1
Thiago <thiagolduarte121@gmail.com>
2025-03-27 17:06:21 -0300

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/coffee-delivery/',
+  base: process.env.NODE_ENV === 'production' ? '/coffee-delivery/' : '/',
   plugins: [react()],
 })
